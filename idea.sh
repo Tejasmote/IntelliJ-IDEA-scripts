4d48d07f53e10c8ac51d47bf8e71b7a6868b9e8a
Tejasmote <51282371+Tejasmote@users.noreply.github.com>
2021-07-22 00:06:25 +0530

    Update idea.sh

--- idea.sh
+++ idea.sh
@@ -9,7 +9,7 @@ echo "Installation Started..."
 sudo tar -xvf ideaIC-2021.1.2.tar.gz -C /opt/
 sudo mv /opt/idea-IC-211.7442.40 /opt/idea
 sudo ln -s /opt/idea/bin/idea.sh /usr/bin/idea
-sudo mv idea.desktop /usr/share/applications/
+sudo cp idea.desktop /usr/share/applications/
 sudo desktop-file-install /usr/share/applications/idea.desktop
 echo "Installation Completed..."
 
